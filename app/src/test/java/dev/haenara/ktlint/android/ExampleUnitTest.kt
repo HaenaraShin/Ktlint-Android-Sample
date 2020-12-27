1a109be6b5093ea04ec08670766155446e8908d2
HaenaraShin <hamster12345@gmail.com>
2020-12-27 22:00:37 +0900

    🛠 fix(logic): fix codes to follow kotlin convention

--- app/src/test/java/dev/haenara/ktlint/android/ExampleUnitTest.kt
+++ app/src/test/java/dev/haenara/ktlint/android/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package dev.haenara.ktlint.android
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
